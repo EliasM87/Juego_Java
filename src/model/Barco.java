891a5b7b52fdb3e947708dc04aba8ce36ce7ed2b
EliasM <tecnicosonidomadrid@gmail.com>
2025-12-18 19:25:25 +0100

    Modificacion clase Barco

--- src/model/Barco.java
+++ src/model/Barco.java
@@ -1,5 +1,62 @@
 package model;
 
 public class Barco {
+	
+	private String nombre;
+	private int longitud;
+	private int impactos;
+	private boolean hundido;
+	
+	public Barco(String nombre, int longitud) {
+		super();
+		this.nombre = nombre;
+		this.longitud = longitud;
+		this.impactos = 0;
+		
+	}
+	public Barco() {
+		super();
+	}
+	
+	
+	public boolean estaHundido() {
+		return impactos >= longitud;
+	}
+	
+	public void registrarImpactos() {
+		this.impactos++;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getLongitud() {
+		return longitud;
+	}
+	public void setLongitud(int longitud) {
+		this.longitud = longitud;
+	}
+	public int getImpactos() {
+		return impactos;
+	}
+	public void setImpactos(int impactos) {
+		this.impactos = impactos;
+	}
+	public boolean isHundido() {
+		return hundido;
+	}
+	public void setHundido(boolean hundido) {
+		this.hundido = hundido;
+	}
+	@Override
+	public String toString() {
+		return "Barco [nombre=" + nombre + ", longitud=" + longitud + ", impactos=" + impactos + ", hundido=" + hundido
+				+ "]";
+	}
+	
+	
 
 }
--- src/model/Casilla.java
+++ src/model/Casilla.java
@@ -1,5 +1,72 @@
 package model;
 
 public class Casilla {
+	
+	private int fila;
+	private int columna;
+	private boolean ocupada;
+	private String simbolo;
+	
+	
+	public Casilla(int fila, int columna, boolean ocupada, String simbolo) {
+		super();
+		this.fila = fila;
+		this.columna = columna;
+		this.ocupada = ocupada;
+		this.simbolo = simbolo;
+	}
+
+
+	public Casilla() {
+		super();
+	}
+
+
+	public int getFila() {
+		return fila;
+	}
+
+
+	public void setFila(int fila) {
+		this.fila = fila;
+	}
+
+
+	public int getColumna() {
+		return columna;
+	}
+
+
+	public void setColumna(int columna) {
+		this.columna = columna;
+	}
+
+
+	public boolean isOcupada() {
+		return ocupada;
+	}
+
+
+	public void setOcupada(boolean ocupada) {
+		this.ocupada = ocupada;
+	}
+
+
+	public String getSimbolo() {
+		return simbolo;
+	}
+
+
+	public void setSimbolo(String simbolo) {
+		this.simbolo = simbolo;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Casilla [fila=" + fila + ", columna=" + columna + ", ocupada=" + ocupada + ", simbolo=" + simbolo + "]";
+	}
+	
+	
 
 }
